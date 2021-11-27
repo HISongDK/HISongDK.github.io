a7fc95b75af946f5ccc90184188d06d2fef0215d
宋德康 <1494403731@qq.com>
2021-11-27 23:58:54 +0800

    修改自动打开新建文件调用vscode路径

--- scripts/openNewFile.js
+++ scripts/openNewFile.js
@@ -1,4 +1,4 @@
 var spawn = require('child_process').spawn;
 hexo.on('new', function(data){
-    spawn('C:\\Users\\14944\\AppData\\Local\\Programs\\Microsoft VS Code\\bin\\code.cmd', [data.path]);
+    spawn('C:\\Users\\song\\AppData\\Local\\Programs\\Microsoft VS Code\\bin\\code.cmd', [data.path]);
 });
---
+++ source/_posts/2021-11-27-Write-Down-Everything.md
@@ -0,0 +1,6 @@
+---
+title: Write Down Everything
+date: 2021-11-27 23:56:46
+tags:
+categories:
+---
