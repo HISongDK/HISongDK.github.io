67344ba95f9f7747789d0398c5766cb2e9db064c
Song Dekang <1494403731@qq.com>
2022-04-05 12:55:41 +0800

    :construction_worker: hexo 新建文件自动打开修改

--- scripts/openNewFile.js
+++ scripts/openNewFile.js
@@ -1,4 +1,4 @@
-var spawn = require('child_process').spawn;
-hexo.on('new', function(data){
-    spawn('C:\\Users\\song\\AppData\\Local\\Programs\\Microsoft VS Code\\bin\\code.cmd', [data.path]);
-});
+var spawn = require('child_process').spawn
+hexo.on('new', function (data) {
+    spawn('C:\\Users\\song\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe', [data.path])
+})
